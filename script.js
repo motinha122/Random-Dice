cdc12df628bc742ecb47c6c321508cb232684a21
motinha122 <timota132@gmail.com>
2022-05-24 21:41:04 -0300

    Random dice script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("a")
+//Random dice number generator
+
+function randomInt(min,max)
+{
+    return Math.floor(Math.random()*(max - min +1 )+ min)
+}
+
+let dice1 = randomInt(1,6);
+let dice2 = randomInt(1,6);
+
+document.getElementById("dice-p1").src="/images/dado-"+dice1+".png";
+document.getElementById("dice-p2").src="/images/dado-"+dice2+".png";
